b899bacd547e86adf9d3b4854c51eb5f5525c497
Emre AKTÜRK <eakturk61@hotmail.com>
2017-04-01 15:27:46 +0300

    Update settings.py

--- emreakturk/settings.py
+++ emreakturk/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -24,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 ALLOWED_HOSTS = ['']
-
 
 #Info
 ADMINS = ('Emre AKTÜRK', 'mail@emreakturk.com')
