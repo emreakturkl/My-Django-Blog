6f275063cf022d991f6e34e7e8e9534e18dbf807
emreakturkk <eakturk61@hotmail.com>
2018-02-01 01:00:05 +0200

    update

--- emreakturk/settings.py
+++ emreakturk/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_files")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
